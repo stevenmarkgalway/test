1cf29638a55fa42a558770fe96811fd10224ce15
stevenmarkgalway <32261372+stevenmarkgalway@users.noreply.github.com>
2017-09-25 11:03:51 +0100

    Add files via upload

---
+++ app.R
@@ -0,0 +1,50 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+
+library(shiny)
+## Only run examples in interactive R sessions
+if (interactive()) {
+  
+  ui <- fluidPage(
+    sidebarLayout(
+      sidebarPanel(
+        fileInput("file1", "choose.csv file",
+                  accept = c(
+                    "text/csv",
+                    "text/comma-separated-values,text/plain",
+                    ".csv")
+        ),
+        tags$hr(),
+        checkboxInput("header", "Header", TRUE)
+      ),
+      mainPanel(
+        tableOutput("contents")
+      )
+    )
+  )
+  
+  server <- function(input, output) {
+    output$contents <- renderTable({
+      # input$file1 will be NULL initially. After the user selects
+      # and uploads a file, it will be a data frame with 'name',
+      # 'size', 'type', and 'datapath' columns. The 'datapath'
+      # column will contain the local filenames where the data can
+      # be found.
+      inFile <- input$file1
+      
+      if (is.null(inFile))
+        return(NULL)
+      
+      read.csv(inFile$datapath, header = input$header)
+    })
+  }
+  
+  shinyApp(ui, server)
+}
